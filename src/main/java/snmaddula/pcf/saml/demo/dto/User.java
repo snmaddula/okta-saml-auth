dbab959b5b29888a9c2007628869156c59472317
SN MADDULA <snmaddula@outlook.com>
2019-02-15 11:10:47 +0530

    Create User.java

---
+++ src/main/java/snmaddula/pcf/saml/demo/dto/User.java
@@ -0,0 +1,12 @@
+package snmaddula.pcf.saml.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String userType;
+}
