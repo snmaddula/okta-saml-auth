d93fbf09cdcf0ecfc3182b8ac37511e6519127d8
SN MADDULA <snmaddula@outlook.com>
2019-02-15 11:11:06 +0530

    Update AppController.java

--- src/main/java/snmaddula/pcf/saml/demo/controller/AppController.java
+++ src/main/java/snmaddula/pcf/saml/demo/controller/AppController.java
@@ -1,12 +1,13 @@
 package snmaddula.pcf.saml.demo.controller;
 
-import java.security.Principal;
-
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.saml.SAMLCredential;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import snmaddula.pcf.saml.demo.dto.User;
 
 @Controller
 public class AppController {
@@ -16,11 +17,16 @@ public class AppController {
 		return "home";
 	}
 
-	@ModelAttribute("username")
-	public String username() {
+	@ModelAttribute("user")
+	public User username() {
 		try {
-			Principal principal = (Principal) SecurityContextHolder.getContext().getAuthentication();
-			return principal.getName();
+			SAMLCredential cred = (SAMLCredential) SecurityContextHolder.getContext().getAuthentication().getCredentials();
+			User user = new User();
+			user.setFirstName(cred.getAttributeAsString("First Name"));
+			user.setLastName(cred.getAttributeAsString("Last Name"));
+			user.setEmail(cred.getAttributeAsString("Email"));
+			user.setUserType(cred.getAttributeAsString("User Type"));
+			return user;
 		} catch (AuthenticationCredentialsNotFoundException ex) {
 			return null;
 		}
